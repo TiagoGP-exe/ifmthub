b7e76bba3995d8267b36ff7ed1b59375b6339976
Tiago Guimarães Pinto <tguimaraespinto@gmail.com>
2023-08-26 21:19:23 -0400

    refactor: adjustment

--- app/dashboard/search/page.tsx
+++ app/dashboard/search/page.tsx
@@ -4,23 +4,14 @@ import { useRouter } from 'next/navigation'
 import { Input } from '../../../components/ui/input'
 
 export default function Home() {
-  const { push } = useRouter()
-
-  const hundred = Array.from(Array(100).keys())
 
   return (
-
     <main className='xs:px-8 flex flex-col'>
       <div className='sticky py-3 top-[4.5rem] bg-background'>
         <Input
           placeholder='Pesquisar'
-          icon='search'
-          size='xs'
-          onChange={() => { }}
         />
       </div>
-
-
     </main>
   )
 }
