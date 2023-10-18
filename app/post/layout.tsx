a7fdb3ca8270625bda231e0f987200a648be9cfa
Tiago Guimarães Pinto <tguimaraespinto@gmail.com>
2023-10-17 20:45:53 -0400

    Update layout.tsx

--- app/post/layout.tsx
+++ app/post/layout.tsx
@@ -1,26 +1,9 @@
-"use client"
-
-import { useEffect } from 'react'
-import { useAuth } from '../../components/use-auth'
-import { useRouter } from 'next/navigation'
-
 interface EditorProps {
   children?: React.ReactNode
 }
 
 export default function EditorLayout({ children }: EditorProps) {
-  const { user, isLoading } = useAuth()
-  const { push } = useRouter()
-
-  useEffect(() => {
-    if (!user?.email && !isLoading) {
-      push("/login")
-    }
-  }, [isLoading, push, user])
-
-  return user?.email ? (
-    <div className="container mx-auto grid items-start py-8 ">
+  return <div className="container mx-auto grid items-start py-8 ">
       {children}
     </div>
-  ) : null
 }
